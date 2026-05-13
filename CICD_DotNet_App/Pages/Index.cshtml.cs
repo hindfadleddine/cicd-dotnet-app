f3211d28ac5c2eb28eee97312cc0f112186a0b8b
hindfadleddine <fadleddinehind526@gmail.com>
2026-05-13 22:59:44 +0100

    Ajoutez des fichiers projet.

---
+++ CICD_DotNet_App/Pages/Index.cshtml.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CICD_DotNet_App.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
